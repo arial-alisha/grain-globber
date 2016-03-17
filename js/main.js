5e4b858274bd7e97491db9b4648306ac048e66d4
arial-alisha <aria0023@algonquinlive.com>
2016-03-16 21:40:09 -0400

    starting css

--- js/main.js
+++ js/main.js
@@ -6,3 +6,11 @@ grains.forEach(function (grain) {
   var $img = $('<img>');
   var $h2 = $('<h2>');
   var $p = $('<p>');
+
+  $h2.html(grain.name);
+  $img.attr('src', 'images/' + grain.img);
+  $p.html(grain.desc);
+
+  $li.append($img, $h2, $p);
+  $ul.append($li);
+});
