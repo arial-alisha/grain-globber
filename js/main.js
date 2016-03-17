7d927218ff3ca47c8a039028fa63ff0f764a760d
arial-alisha <aria0023@algonquinlive.com>
2016-03-16 21:43:04 -0400

    float, markbot fixes

--- js/main.js
+++ js/main.js
@@ -8,9 +8,10 @@ grains.forEach(function (grain) {
   var $p = $('<p>');
 
   $h2.html(grain.name);
-  $img.attr('src', 'images/' + grain.img);
+  $img.attr('src', 'images/' + 'grain.img');
   $p.html(grain.desc);
 
   $li.append($img, $h2, $p);
   $ul.append($li);
+
 });
