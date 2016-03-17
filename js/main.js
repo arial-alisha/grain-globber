c54eec3a6a1bdb99f5106616a76cac56fe25bbc0
arial-alisha <aria0023@algonquinlive.com>
2016-03-16 21:43:32 -0400

    no quote there

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ grains.forEach(function (grain) {
   var $p = $('<p>');
 
   $h2.html(grain.name);
-  $img.attr('src', 'images/' + 'grain.img');
+  $img.attr('src', 'images/' + grain.img);
   $p.html(grain.desc);
 
   $li.append($img, $h2, $p);
